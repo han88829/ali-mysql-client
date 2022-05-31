18979ac257c1fb045d116d23be0413249072a64f
han88829 <h88829@foxmail.com>
2022-05-31 08:40:50 +0000

    Deploying to master from  @ d7b2101350d6a887dd8fd0b0fb1a9efd3945b994 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/lib/builder/update.js
@@ -0,0 +1,49 @@
+'use strict';
+
+class UpdateBuilder {
+  constructor(provider, table, data) {
+    this.provider = provider;
+    this.data = {
+      table,
+      data: data || {},
+      where: [],
+    };
+  }
+
+  column(name, value) {
+    this.data.data[name] = value;
+
+    return this;
+  }
+
+  where(field, value, operator, ignore, join) {
+    this.data.where.push(
+      typeof field === 'object'
+        ? field
+        : { field, value, operator, ignore, join }
+    );
+
+    return this;
+  }
+  inc(name, value) {
+    this.data.data[name] = this.provider.command.literals.Literal(`${name}+${value}`);
+    return this;
+  }
+  dec(name, value) {
+    this.data.data[name] = this.provider.command.literals.Literal(`${name}-${value}`);
+    return this;
+  }
+  execute() {
+    return this.provider
+      .parseUpdate(this.data)
+      .execute();
+  }
+
+  toSql() {
+    return this.provider
+      .parseUpdate(this.data)
+      .format();
+  }
+}
+
+module.exports = UpdateBuilder;
